da9b1c90f89abe93179fff128b7269a22797da79
InfernoDragon0 <mushroomapplepiele@gmail.com>
2023-08-06 10:30:40 +0800

    hover example

--- app.vue
+++ app.vue
@@ -1,62 +1,72 @@
 <template>
-  <!-- Whole Screen -->
-  <div class="flex flex-nowrap h-screen">
-    <!-- Left Header/Sidebar -->
-    <div class="h-screen w-auto mx-0 flex-none bg-gray-900 flex flex-col">
-      <!-- Navigation on Sidebar -->
-      <div class="flex flex-col my-auto">
-        <button class="h-auto w-full">
-          <img src="~/public/sample/sample_logo_256x256.png" class="h-8 w-8 mx-8 my-4 rounded-full" />
-        </button>
-        <button class="h-auto w-full">
-          <img :src="navigationButton" @on-mouseover="navigationButtonMouseover"
-            class="h-8 w-8 mx-8 my-4 rounded-full transition ease-linear hover:scale-110" />
-        </button>
-        <button class="h-auto w-full">
-          <img :src="navigationButton" class="h-8 w-8 mx-8 my-4 rounded-full transition ease-linear hover:scale-110" />
-        </button>
-        <button class="h-auto w-full">
-          <img :src="navigationButton" class="h-8 w-8 mx-8 my-4 rounded-full transition ease-linear hover:scale-110" />
-        </button>
-        <button class="h-auto w-full">
-          <img :src="navigationButton" class="h-8 w-8 mx-8 my-4 rounded-full transition ease-linear hover:scale-110" />
-        </button>
+    <!-- Whole Screen -->
+    <div class="flex flex-nowrap h-screen">
+        <!-- Left Header/Sidebar -->
+        <div class="h-screen w-auto mx-0 flex-none bg-gray-900 flex flex-col">
+            <!-- Navigation on Sidebar -->
+            <div class="flex flex-col my-auto">
+                <button class="h-auto w-full">
+                    <img src="~/public/sample/sample_logo_256x256.png" class="h-8 w-8 mx-8 my-4 rounded-full" />
+                </button>
 
-      </div>
+                <button @mouseover="mousingOver = true" @mouseleave="mousingOver = false"
+                    class="btn btn-ghost h-auto w-full transition ease-linear hover:scale-110">
 
-      <!-- Login/User/Profile Button -->
-      <button class="h-auto w-full">
-        <img src="~/public/sample/sample_profile_picture.png" class="h-8 w-8 m-8 rounded-full" />
-      </button>
+                    <img :src="mousingOver ? navigationButtonMouseover : navigationButton" class="h-8 w-8 mx-8 my-4" />
+
+                </button>
+
+                <button class="btn btn-ghost h-auto w-full transition ease-linear hover:scale-110">
+                    <img :src="navigationButton" class="h-8 w-8 mx-8 my-4" />
+                </button>
+                <button class="btn btn-ghost h-auto w-full transition ease-linear hover:scale-110">
+                    <img :src="navigationButton" class="h-8 w-8 mx-8 my-4" />
+                </button>
+                <button class="btn btn-ghost h-auto w-full transition ease-linear hover:scale-110">
+                    <img :src="navigationButton" class="h-8 w-8 mx-8 my-4" />
+                </button>
+
+            </div>
+
+            <!-- Login/User/Profile Button -->
+            <button class="h-auto w-full">
+                <img src="~/public/sample/sample_profile_picture.png" class="h-8 w-8 m-8 rounded-full" />
+            </button>
+        </div>
+
+        <!-- Right Content -->
+        <div class="w-auto overflow-y-auto overflow-x-hidden">
+            <NuxtPage />
+        </div>
     </div>
 
-    <!-- Right Content -->
-    <div class="w-auto overflow-y-auto overflow-x-hidden">
-      <NuxtPage />
-    </div>
-  </div>
-
-  <div></div>
+    <div></div>
 </template>
 
 <script setup lang="ts">
 const router = useRouter();
 const route = useRoute();
 const user = useCurrentUser();
+const mousingOver = ref(false);
 
-const navigationButton = "/public/spa.png"
-const navigationButtonMouseover = "/public/sample/sample_logo_256x256.png"
+const navigationButton = "/spa.png"
+const navigationButtonMouseover = "/sample/sample_logo_256x256.png"
 
 // we don't need this watcher on server
 onMounted(() => {
-  watch(user, (user, prevUser) => {
-    if (prevUser && !user) {
-      // user logged out
-      router.push("/login");
-    } else if (user && typeof route.query.redirect === "string") {
-      // user logged in
-      router.push(route.query.redirect);
-    }
-  });
+    watch(user, (user, prevUser) => {
+        if (prevUser && !user) {
+            // user logged out
+            router.push("/login");
+        } else if (user && typeof route.query.redirect === "string") {
+            // user logged in
+            router.push(route.query.redirect);
+        }
+    });
 });
-</script>=
+
+const logtest = () => {
+    console.log("test")
+}
+
+</script>
