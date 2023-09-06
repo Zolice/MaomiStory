3a27ec135864711bca8fc90a2cc984dd8278db88
infernodragon0 <mushroomapplepiele@gmail.com>
2023-09-06 16:14:52 +0800

    Change home to index

--- components/NavigationComponent.vue
+++ components/NavigationComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="backdrop-blur-sm fixed bottom-0 z-50  h-24 w-full pl-8 p-4 flex gap-10 place-items-center">
-        <NuxtLink to="/home">
+        <NuxtLink to="/">
             <button class="btn rounded-full btn-ghost ">
                 <span class="material-symbols-outlined text-primary drop-shadow-[0_1px_10px_rgba(246,211,123,1)]">home</span>
             </button>
---
+++ pages/logoutarea.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Test</h1>
+        <button class="btn" @click="logout">logout</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+// onMounted(() => {
+//     console.log("mounted")
+
+//     //check if user already logged in
+//     const user = useCurrentUser()
+//     if (user.value) {
+//         console.log("user already logged in")
+//         //log user name and email
+//         console.log(user.value?.displayName, user.value?.email)
+//         //redirect to index
+//         return
+//     }
+// })
+
+const logout = () => {
+    console.log("logout");
+    //logout
+    navigateTo("/logout")    
+};
+</script>
