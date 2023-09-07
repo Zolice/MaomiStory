c030f2cabbad3b0ecf5958890b933cd6c94cf1b8
infernodragon0 <mushroomapplepiele@gmail.com>
2023-09-07 15:34:12 +0800

    add filter and cart buttons

--- pages/shop.vue
+++ pages/shop.vue
@@ -3,19 +3,31 @@
         <NuxtImg src="/shopbg.png" class="h-screen w-screen object-cover fixed -z-10" />
         <!-- Title bar -->
         <div class="p-4 sticky top-0 z-10 backdrop-blur-sm">
-            <div class="flex gap-2 p-4 place-items-center">
-                <span class="material-symbols-outlined text-[48px]">spa</span>
-                <p class="text-[42px] font-inter uppercase font-bold tracking-wider leading-[48px] mr-4">Savior</p>
-                <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Coin Shop">
-                    <NuxtImg src="logo.png" class=" h-[32px] w-[32px] object-contain" />
-                </div>
-                <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Coin Shop">
-                    <NuxtImg src="logo.png" class=" h-[32px] w-[32px] object-contain" />
-                </div>
-                <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Coin Shop">
-                    <NuxtImg src="logo.png" class=" h-[32px] w-[32px] object-contain" />
+            <div class="flex gap-2 p-4 place-items-center justify-between">
+                <div class="flex gap-2 p-4 place-items-center">
+                    <span class="material-symbols-outlined text-[48px]">spa</span>
+                        <p class="text-[42px] font-inter uppercase font-bold tracking-wider leading-[48px] mr-4">Savior</p>
+                        <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Coin Shop">
+                            <NuxtImg src="logo.png" class=" h-[32px] w-[32px] object-contain" />
+                        </div>
+                        <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Coin Shop">
+                            <NuxtImg src="logo.png" class=" h-[32px] w-[32px] object-contain" />
+                        </div>
+                        <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Coin Shop">
+                            <NuxtImg src="logo.png" class=" h-[32px] w-[32px] object-contain" />
+                        </div>
                 </div>
                 
+                <!-- Filter and Shopping Cart -->
+                <div class="flex">
+                    <div class="tooltip tooltip-bottom btn btn-ghost flex" data-tip="Filter">
+                        <span class="material-symbols-outlined">filter_list</span>
+                    </div>
+                    <div class="tooltip tooltip-bottom btn btn-ghost flex indicator" data-tip="View Cart">
+                        <span class="indicator-item badge badge-primary badge-xs mt-1 mr-1">100</span>
+                        <span class="material-symbols-outlined">shopping_cart</span>
+                    </div>
+                </div>
             </div>
             <p class="pl-4 font-outfit font-light text-gray-400">
                 SAVIOR: Wongstaurant runs from 11/11/1970 to 11/11/2032. 
@@ -32,13 +44,12 @@
             <ShopAccordionComponent/>
         </div>
 
-        <ShopCartBoxComponent/>
+        <div class="toast">
+            <ShopCartBoxComponent/>
+        </div>
 
         <!-- extra space -->
         <div class="h-[175px]">
         </div>
-        
-       
-
     </div>
 </template>
