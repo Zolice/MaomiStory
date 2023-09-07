bcce070d1e91d8fba2d73d4e5a06ed6d957d40bc
infernodragon0 <mushroomapplepiele@gmail.com>
2023-09-07 16:12:26 +0800

    items as buttons

--- components/ShopBoxComponent.vue
+++ components/ShopBoxComponent.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="bg-black/25 flex flex-col justify-evenly place-items-center w-[200px] h-[210px] p-2 text-center rounded-lg">
+    <button class="btn btn-ghost bg-black/25 flex flex-col justify-evenly place-items-center w-[200px] h-[210px] p-2 text-center rounded-lg">
         <NuxtImg src="/logo.png" class="w-[48px] h-[48px] object-contain" />
         <p class="text-white font-light font-outfit w-full line-clamp-2 mb-2">
             Legendary Arcaneshade Weapon Box with long name
         </p>
-        <div class="flex gap-1">
+        <div class="flex gap-1 place-items-center">
             <span class="material-symbols-outlined text-primary text-[22px] place-self-center">monetization_on</span>
             <p class="font-outfit font-bold">9999</p>
         </div>
-        <div class="flex gap-1">
+        <div class="flex gap-1 place-items-center">
             <span class="material-symbols-outlined text-success text-[22px] place-self-center">package_2</span>
             <p class="font-outfit text-success font-light">999</p>
         </div>
         <p class="text-error text-sm">Untradeable</p>
-    </div>
+    </button>
 </template>
